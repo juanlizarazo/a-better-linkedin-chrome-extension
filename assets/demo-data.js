fb09c91ccf01a0b20dc7be42e415d568e2ca1d57
Juan Lizarazo <gabriellg4@gmail.com>
2017-12-28 14:29:40 -0700

    Demo data used to create screenshot.

---
+++ assets/demo-data.js
@@ -0,0 +1,32 @@
+const demoData = [
+  {
+    name: 'Andrea Mitchell',
+    pictureUrl: 'https://randomuser.me/api/portraits/women/58.jpg',
+    subject: 'RE: Following up from awesome company',
+    body: 'Hey, not a problem and I\'ll send a LI invite to connect if/when things change for you! Enjoy you...'
+  },
+  {
+    name: 'Lauren Dixon',
+    pictureUrl: 'https://randomuser.me/api/portraits/women/53.jpg',
+    subject: 'No subject',
+    body: 'Yeah, that is really great! That\'s fair. Would you be open to a new opportunity there, if it was...'
+  },
+  {
+    name: 'Richard Rogers',
+    pictureUrl: 'https://randomuser.me/api/portraits/men/58.jpg',
+    subject: 'Referrals?',
+    body: 'Hi again, How are you? I wanted to see if you would happen to know of anyone looking who would fit...'
+  },
+  {
+    name: 'Victor Davies',
+    pictureUrl: 'https://randomuser.me/api/portraits/men/53.jpg',
+    subject: 'Open to new opportunities?',
+    body: 'I wanted to reach out to you and see what if you are open to new opportunities. I work fo...'
+  },
+  {
+    name: 'Mike Dunn',
+    pictureUrl: 'https://randomuser.me/api/portraits/men/12.jpg',
+    subject: 'No subject',
+    body: 'We thought so. I just gave your number to him so either he or I will reach out to sched...'
+  },
+];
